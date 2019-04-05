916d062cb9182bbef7296aaf37d8fb5f99fcaa9d
solov9ev <a.solov9ev@yandex.ru>
2019-04-05 21:28:00 +0300

    Credo отслеживает папку /test

--- test/faust_web/controllers/page_controller_test.exs
+++ test/faust_web/controllers/page_controller_test.exs
@@ -1,8 +1,30 @@
 defmodule FaustWeb.PageControllerTest do
+  @moduledoc false
+
   use FaustWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  import Faust.Support.Factories
+  import Phoenix.Controller, only: [view_template: 1]
+  import Plug.Conn.Status, only: [code: 1]
+
+  describe "index" do
+    test "get root page when user is not authorized", %{conn: conn} do
+      conn = get(conn, "/")
+
+      assert conn.status == code(:ok)
+      assert view_template(conn) == "index.html"
+    end
+
+    test "show user page when he is authorized", %{conn: conn} do
+      user = insert(:user)
+
+      conn =
+        conn
+        |> authorize_conn(user)
+        |> get(Routes.user_path(conn, :new))
+
+      assert conn.status == code(:found)
+      assert redirected_to(conn) == Routes.user_path(conn, :show, user)
+    end
   end
 end
