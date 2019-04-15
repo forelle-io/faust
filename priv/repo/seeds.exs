d1c091a1f40aecabac2700f9123491fa5385c42b
solov9ev <a.solov9ev@yandex.ru>
2019-04-15 17:40:38 +0300

    Seed для таблицы fishes

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -6,5 +6,5 @@ fishes =
   ~w(белый амур берш голавль густера ерш жерех карась карп карп красноперка лещ линь лосось налим окунь пескарь плотва сазан сом стерлядь судак толстолобик угорь уклейка форель хариус щука язь)
 
 Multi.new()
-|> Multi.insert_all(:insert_all, Fish, Enum.map(fishes, &(%{name: &1})))
+|> Multi.insert_all(:insert_all, Fish, Enum.map(fishes, &%{name: &1}))
 |> Faust.Repo.transaction()
