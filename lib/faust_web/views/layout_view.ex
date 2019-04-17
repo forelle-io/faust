b10797ed64bef9a98b69aa58a6197dc9fb90881d
solov9ev <a.solov9ev@yandex.ru>
2019-04-17 14:12:03 +0300

    FE-26 Динамический рендеринг flash сообщений

--- lib/faust_web/views/layout_view.ex
+++ lib/faust_web/views/layout_view.ex
@@ -1,7 +1,16 @@
 defmodule FaustWeb.LayoutView do
   use FaustWeb, :view
 
-  def logotype_path(conn) do
-    Routes.static_path(conn, "/images/logotype.png")
+  def clickable_logotype(conn) do
+    conn
+    |> Routes.static_path("/images/logotype.png")
+    |> img_tag(height: 72)
+    |> link(
+      to:
+        if(authenticated_user?(conn),
+          do: Routes.user_path(conn, :show, current_user(conn)),
+          else: "/"
+        )
+    )
   end
 end
---
+++ lib/faust_web/views/shared_view.ex
@@ -0,0 +1,5 @@
+defmodule FaustWeb.SharedView do
+  use FaustWeb, :view
+
+  import FaustWeb.LayoutView, only: [clickable_logotype: 1]
+end
