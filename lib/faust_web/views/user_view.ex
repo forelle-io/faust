37a3b5a7b0cf716f381f03f039037693257ac08c
solov9ev <a.solov9ev@yandex.ru>
2019-06-06 00:04:18 +0300

    Удаление устаревших функций, генерирующих ссылки Отписаться / Подписаться в FaustWeb.UserView

--- lib/faust_web/views/user_view.ex
+++ lib/faust_web/views/user_view.ex
@@ -1,25 +1,3 @@
 defmodule FaustWeb.UserView do
   use FaustWeb, :view
-
-  alias Faust.Accounts.User
-
-  def follower_link(current_followee_ids, %User{id: id}) when is_list(current_followee_ids) do
-    if Enum.member?(current_followee_ids, id) do
-      link("Отписаться",
-        to: "#",
-        class: "btn btn-primary",
-        id: id,
-        onclick: "follow_user(\"#{Plug.CSRFProtection.get_csrf_token()}\", this);",
-        data: [action: "unfollow"]
-      )
-    else
-      link("Подписаться",
-        to: "#",
-        class: "btn btn-outline-primary",
-        id: id,
-        onclick: "follow_user(\"#{Plug.CSRFProtection.get_csrf_token()}\", this);",
-        data: [action: "follow"]
-      )
-    end
-  end
 end
