38b6959d1d2e3e9e8648a8681c2936c27eeb291b
solov9ev <a.solov9ev@yandex.ru>
2019-07-19 21:43:00 +0300

    Рефакторинг

--- lib/faust_web/policies/reservoir/history_policy.ex
+++ lib/faust_web/policies/reservoir/history_policy.ex
@@ -2,8 +2,5 @@ defmodule FaustWeb.Reservoir.HistoryPolicy do
   @moduledoc false
   @behaviour Bodyguard.Policy
 
-  alias Faust.Accounts.{Chief, Organization}
-  alias Faust.Reservoir.Water
-
   def authorize(_, _, _), do: false
 end
