d0bf6bdfe5009f05ac37ab1acc0053b6105920aa
solov9ev <a.solov9ev@yandex.ru>
2019-08-22 23:52:33 +0300

    Bump credo from 1.1.2 to 1.1.3

--- mix.exs
+++ mix.exs
@@ -70,7 +70,7 @@ defmodule Faust.MixProject do
       {:distillery, "~> 2.0.14", warn_missing: false},
       # Тестирование, инспекция кода, фикстуры, фабрики
       {:excoveralls, "~> 0.10", only: :test},
-      {:credo, "~> 1.1.2", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.1.3", only: [:dev, :test], runtime: false},
       {:ex_machina, "~> 2.3", only: :test}
     ]
   end
